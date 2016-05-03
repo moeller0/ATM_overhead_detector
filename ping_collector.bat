058364a69204c89ef1c7944f42f9ddee6f8ba5f0
Sebastian Moeller <moeller0@gmx.de>
2016-05-03 17:37:53 +0200

    Supply a small batch file to use hrping on windows systems
    Since not all users have unix machines with working GNU/BSD
    utiities available, this commit adds a small script to
    use hrping (see https://www.cfos.de/en/ping/ping.htm )to
    collect the ICMP probe data for the 2nd stage matlab/octave script.
    Requires proper testing...
    
    Signed-off-by: Sebastian Moeller <moeller0@gmx.de>

---
+++ ping_collector.bat
@@ -0,0 +1,63 @@
+@ECHO OFF
+
+SETLOCAL
+
+REM this could be smarter...
+
+REM try to run hrping to collect data to investigate
+
+REM whether a link uses ATM cell quantization
+
+
+
+
+REM Is hrping installed? Bail out and complain if not
+
+REM Is hrping installed? Bail out & complain if not
+
+REM Get hrping from: https://www.cfos.de/en/ping/ping.htm
+REM hrping required EULA confirmation on initial run
+
+ECHO This script requires administrative priviledge
+
+
+REM Define variables
+REM gstatic.com does seem to respond only up to 64 bytes ICMP payload...
+
+SET TARGET=8.8.8.8
+
+SET /A SWEEPMINSIZE=16
+SET /A SWEEP_N_ATM_CELLS=3
+SET /A SWEEPMAXSIZE=%SWEEP_N_ATM_CELLS% * 48 + %SWEEPMINSIZE%
+
+SET PINGPERIOD_MS=10
+SET PINGSPERSIZE=1000
+
+
+SET /A N_SIZES=%SWEEPMAXSIZE% - %SWEEPMINSIZE%
+ECHO %N_SIZES%
+SET /A N_PINGSTOTAL=%N_SIZES% * %PINGSPERSIZE%
+ECHO %N_PINGSTOTAL%
+
+SET TECH=xDSL
+
+REM get time and date and remove some unwanted characters
+SET TIMESTR=%TIME: =%
+SET TIMESTR=%TIMESTR::=%
+SET DATESTR=%DATE: =%
+
+SET DATESTR=%DATESTR:/=%
+
+SET LOG_FILE_NAME=ping_sweep_%TECH%_%DATESTR%_%TIMESTR%.txt
+
+SET PING_CMD=hrping -q -l%SWEEPMINSIZE%:%SWEEPMAXSIZE%:1 -s %PINGPERIOD_MS% -W -n %N_PINGSTOTAL% -F %LOG_FILE_NAME% %TARGET%
+
+ECHO %PING_CMD%
+
+
+REM now run this
+%PING_CMD%
+
+ECHO DONE
+ENDLOCAL
+ECHO ON
